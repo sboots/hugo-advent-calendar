d01891539b982330f8b5b4d57ef74d22d3050cc1
Sean Boots <sean.boots@gmail.com>
2020-11-24 19:52:58 -0700

    Initial Advent Calendar layout files

---
+++ content/_index.md
@@ -0,0 +1,6 @@
+---
+title: "2020 Advent Calendar"
+date: 2020-11-23T21:09:41-07:00
+draft: false
+---
+
---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+// Page JS for the Advent Calendar
+// Created 2020-11-24
+
+var app = app || {};
+
+$(function () {
+  // Initial page load config
+  console.log("Running!");
+});
